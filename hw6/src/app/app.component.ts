b71fc9f0ae7802e10e9616b44dff89e67ffe9811
Dmitry Mikutsky <dmitry.mikutsky@gmail.com>
2019-10-29 13:52:38 +0200

    Hoemwork 6 completed. I think so...

--- hw6/src/app/app.component.ts
+++ hw6/src/app/app.component.ts
@@ -4,13 +4,11 @@ import { Component } from "@angular/core";
   selector: "app-root",
   template: `
     <h1>Homework №{{ hwNumber }}</h1>
-    <task-text-component></task-text-component>
-    <img-control-component></img-control-component>
+    <task-text></task-text>
+    <img-control></img-control>
   `,
   styleUrls: ["./app.component.css"]
 })
-class AppComponent {
+export class AppComponent {
   public hwNumber: number = 6;
 }
-
-export { AppComponent };
